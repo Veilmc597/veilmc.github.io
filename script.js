64e1eccde17c87d2d67854be641ce57a17d79f95
Veilmc597 <veilmc.pl@gmail.com>
2025-12-28 01:17:28 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,10 @@
-// cień paska po scrollu
-window.addEventListener("scroll", () => {
-    const nav = document.querySelector(".navbar");
-    nav.classList.toggle("scrolled", window.scrollY > 10);
-});
-
-// kopiowanie IP
 function copyIP() {
     navigator.clipboard.writeText("veilmc.pl");
-    alert("IP skopiowane: veilmc.pl");
+    const el = document.getElementById("ip");
+    el.classList.add("copied");
+    el.innerText = "Skopiowano!";
+    setTimeout(() => {
+        el.classList.remove("copied");
+        el.innerText = "IP: veilmc.pl (kliknij aby skopiować)";
+    }, 1500);
 }
